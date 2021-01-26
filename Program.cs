9fa6b43ddd176d9c4a2210532ed6c212ef6130a9
Matthew Potter <potter8@usf.edu>
2021-01-26 09:36:03 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿/*Creater: Matthew Potter
+ * 01/29/2021
+ * IterativeStatement
+ */
+using System;
+
+namespace IterativeStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an interger value between 1 and 100");
+
+            try
+            {
+                string input = Console.ReadLine();
+                int value_of_input = int.Parse(input);
+
+                if ((value_of_input >= 1) && (value_of_input <= 100))
+                {
+                    for (int i = 1; i <= value_of_input; i++)
+                    {
+                        Console.WriteLine("You have entered" + " " + value_of_input.ToString() + "." + " " +
+                          "This is the current integer value in the loop :" + i.ToString());
+                    }
+                }
+
+            }
+            catch 
+            {
+                Console.WriteLine("Please enter an interger between 1 and 100");
+                Console.WriteLine("Please press any key to exit and try again");
+            }
+        }
+    }
+}
